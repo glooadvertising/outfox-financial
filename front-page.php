4c42c71f768de3ee35e7cac13465f4fc116097da
Pablo Sazo <pablo@glooadvertising.com.au>
2023-11-28 16:40:08 +1100

    initial commit

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+<?php get_template_part( 'partials/tail' ); ?>
+<main>
+    <section class="home-hero">
+        <div class="container">
+            <h2>
+                <?php bloginfo('description'); ?>
+            </h2>
+            <span class="box-row">
+            <a href="#contact-us" class="btn">
+                Contact us
+            </a>
+            <a href="#" class="btn">
+                book appointment
+            </a>
+            </span>
+        </div>
+    </section>
+    <?php get_template_part( 'partials/contact' ); ?>
+</main>
+<?php get_footer(); ?>
---
+++ partials/contact.php
@@ -0,0 +1,19 @@
+<section id="contact-us" class="contact">
+    <div class="container">
+        <div class="col-one">
+            <?php echo do_shortcode('[fluentform id="1"]'); ?>
+        </div>
+        <div class="col-two">
+            <div class="contact-details">
+                <p><strong>Michael Fox MFinPlan, CFP®, LRS®, BBus</strong><br>
+                Financial Advisor<br><br>
+                <a href="tel:0400-765-575">0400 765 575</a><br>
+                <a href="mailto:michael@outfoxfinancial.com.au">michael@outfoxfinancial.com.au</a><br><br><br>
+                <p>
+                Outfox Financial is a Corporate Authorised Representative (No. xxx) and Michael Fox is an Authorised Representative (No. 1265154) of Spark Advisors Australia Pty Ltd ABN 34 122 486 935 AFSL 380552.
+                </p>
+            </p>
+            </div>
+        </div>
+    </div>
+</section>
