86bfb03c7b37935f3ad2593a7d3ae12373a9e1d6
Pablo Sazo <pablo@glooadvertising.com.au>
2023-12-04 16:06:09 +1100

    tel

--- partials/contact.php
+++ partials/contact.php
@@ -7,7 +7,7 @@
             <div class="contact-details">
                 <p><strong>Michael Fox MFinPlan, CFP®, LRS®, BBus</strong><br>
                 Financial Advisor<br><br>
-                <a href="tel:0400-765-575">0400 765 575</a><br>
+                <a href="tel:0459-657-273">0459 657 273</a><br>
                 <a href="mailto:michael@outfoxfinancial.com.au">michael@outfoxfinancial.com.au</a><br><br><br>
                 <p>
                 <?php bloginfo('title'); ?> is a Corporate Authorised Representative (No. 001307040) and Michael Fox is an Authorised Representative No. 001265154 of Spark Advisors Australia Pty Ltd ABN 34 122 486 935 AFSL 380552.
