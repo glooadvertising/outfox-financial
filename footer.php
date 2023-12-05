6c1d525dbf1bec8df3739bba6448d0f59749860c
Pablo Sazo <pablo@glooadvertising.com.au>
2023-12-05 15:48:03 +1100

    Download doc

--- footer.php
+++ footer.php
@@ -4,6 +4,8 @@
                 <a href="https://www.sparkfg.com.au/privacy-policy/" target="_blank">Privacy Policy</a>
                  | 
                 <a href="https://www.sparkfg.com.au/public-complaints-policy/" target="_blank">Public Complaints Policy</a>
+                 | 
+                <a href="/wp-content/uploads/2023/12/Outfox-Financial-Pty-Ltd-FSG-v01.23-MFv1.pdf" download>Financial Services Guide</a>
             </span>
             <p>Site by <a href="https://glooadvertising.com.au" target="_blank">Gloo</a></p>
         </div>
